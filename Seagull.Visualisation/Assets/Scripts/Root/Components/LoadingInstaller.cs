582c9f774e83e17d6021c6b7a2147e52c7067bf4
BeardedPlatypus <mwtegelaers@gmail.com>
2021-05-30 08:49:03 +0200

    Add documentation to LoadingInstaller

--- Seagull.Visualisation/Assets/Scripts/Root/Components/LoadingInstaller.cs
+++ Seagull.Visualisation/Assets/Scripts/Root/Components/LoadingInstaller.cs
@@ -3,8 +3,14 @@ using Zenject;
 
 namespace Seagull.Visualisation.Root.Components
 {
-    public class LoadingInstaller : MonoInstaller
+    /// <summary>
+    /// <see cref="LoadingInstaller"/> defines the dependencies related to Loading.
+    /// </summary>
+    public sealed class LoadingInstaller : MonoInstaller
     {
+        /// <summary>
+        /// The <see cref="sceneTransitionManager"/> of the scene.
+        /// </summary>
         public SceneTransitionManager sceneTransitionManager;
         
         public override void InstallBindings()
