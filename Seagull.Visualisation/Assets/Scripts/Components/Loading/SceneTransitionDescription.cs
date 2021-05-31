e71ca44dcddf37d274e4f547b3d43a4f655d7af2
BeardedPlatypus <mwtegelaers@gmail.com>
2021-05-31 18:40:32 +0200

    Add SceneTransitionDescription

---
+++ Seagull.Visualisation/Assets/Scripts/Components/Loading/SceneTransitionDescription.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace Seagull.Visualisation.Components.Loading
+{
+    public class SceneTransitionDescription : ISceneTransitionDescription
+    {
+        public SceneTransitionDescription(string sceneName, 
+                                          IEnumerator preSceneLoadCoroutine, 
+                                          IEnumerator postSceneLoadCoroutine)
+        {
+            SceneName = sceneName;
+            PreSceneLoadCoroutine = preSceneLoadCoroutine;
+            PostSceneLoadCoroutine = postSceneLoadCoroutine;
+        }
+        
+        public string SceneName { get; }
+        public IEnumerator PreSceneLoadCoroutine { get; }
+        public IEnumerator PostSceneLoadCoroutine { get; }
+    }
+}
