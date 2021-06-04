e798922e3e23ea8518f260ed63c072c67ab9bdb8
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-04 23:05:32 +0200

    Document the NewProjectPage.State

--- Seagull.Visualisation/Assets/Scripts/Views/MainMenu/NewProjectPage/State.cs
+++ Seagull.Visualisation/Assets/Scripts/Views/MainMenu/NewProjectPage/State.cs
@@ -4,14 +4,30 @@ using Zenject;
 
 namespace Seagull.Visualisation.Views.MainMenu.NewProjectPage
 {
+    /// <summary>
+    /// <see cref="State"/> implements the state of a <see cref="NewProjectPage"/>.
+    /// </summary>
     public sealed class State
     {
+        /// <summary>
+        /// The path to the project file to create.
+        /// </summary>
         [CanBeNull] public IPath ProjectPath { get; set; } = null;
+        
+        /// <summary>
+        /// The path to the map file to load.
+        /// </summary>
         [CanBeNull] public IPath MapFilePath { get; set; } = null;
 
+        /// <summary>
+        /// Whether a new project should be created in a new directory.
+        /// </summary>
         public bool ShouldCreateNewSolutionDirectory { get; private set; } = false;
 
-        // TODO: move this to the controller
+        /// <summary>
+        /// Configure whether a new project should be created in a new directory.
+        /// </summary>
+        /// <param name="newValue">Whether to create a new directory.</param>
         public void ConfigureCreateNewSolutionDirectory(bool newValue)
         {
             if (newValue == ShouldCreateNewSolutionDirectory)
@@ -31,7 +47,9 @@ namespace Seagull.Visualisation.Views.MainMenu.NewProjectPage
         private static IPath RemoveProjectDirToPath(IPath path) =>
             path.Parent(2).Join(path.Filename);
 
-        
+        /// <summary>
+        /// <see cref="Factory"/> implements a factory to create <see cref="State"/> objects.
+        /// </summary>
         public class Factory : PlaceholderFactory<State> { }
     }
 }
