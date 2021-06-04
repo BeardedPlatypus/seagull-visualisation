e22e0726dd07eb15dfbf514d4a18813936f10f16
BeardedPlatypus <mwtegelaers@gmail.com>
2021-06-04 22:58:29 +0200

    Document the NewProjectPage.Bindings

--- Seagull.Visualisation/Assets/Scripts/Views/MainMenu/NewProjectPage/Bindings.cs
+++ Seagull.Visualisation/Assets/Scripts/Views/MainMenu/NewProjectPage/Bindings.cs
@@ -4,11 +4,32 @@ using UnityEngine.UI;
 
 namespace Seagull.Visualisation.Views.MainMenu.NewProjectPage
 {
+    /// <summary>
+    /// <see cref="Bindings"/> implements components accessible to a controller
+    /// to visualise the state.
+    /// </summary>
     public class Bindings : MonoBehaviour
     {
+        /// <summary>
+        /// The <see cref="FileSelectionRowController"/> describing the project 
+        /// location.
+        /// </summary>
         public FileSelectionRowController projectLocation;
+        
+        /// <summary>
+        /// The <see cref="FileSelectionRowController"/> describing the map file
+        /// location.
+        /// </summary>
         public FileSelectionRowController mapFileLocation;
+        
+        /// <summary>
+        /// The <see cref="Button"/> to return to the <see cref="OpeningPage"/>.
+        /// </summary>
         public Button backButton;
+        
+        /// <summary>
+        /// The <see cref="Button"/> to create a new project.
+        /// </summary>
         public Button createProjectButton;
     }
 }
