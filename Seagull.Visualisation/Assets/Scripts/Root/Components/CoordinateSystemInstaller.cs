eb5ab1d906461c3baf23ac0700dda3cb60daf951
BeardedPlatypus <mwtegelaers@gmail.com>
2021-08-01 10:27:04 +0200

    Add a CoordinateSystemInstaller for coordinate system dependencies

---
+++ Seagull.Visualisation/Assets/Scripts/Root/Components/CoordinateSystemInstaller.cs
@@ -0,0 +1,20 @@
+﻿using Seagull.Visualisation.Core.Application.Model;
+using Seagull.Visualisation.Core.Infrastructure.Model;
+using Zenject;
+
+namespace Seagull.Visualisation.Root.Components
+{
+    /// <summary>
+    /// <see cref="CoordinateSystemInstaller"/> provides the dependencies related to
+    /// interacting with coordinate systems / EPSG codes.
+    /// </summary>
+    public class CoordinateSystemInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<ICoordinateSystemTransformationService>()
+                     .To<CoordinateSystemTransformationService>()
+                     .AsSingle();
+        }
+    }
+}
