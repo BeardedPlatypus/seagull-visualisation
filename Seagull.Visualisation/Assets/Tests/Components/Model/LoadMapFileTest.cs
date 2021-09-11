206264e0dc3ac1e02696405177a4ed407638d659
BeardedPlatypus <mwtegelaers@gmail.com>
2021-09-11 23:09:20 +0200

    Add NetCDF tests

--- Seagull.Visualisation/Assets/Tests/Components/Model/LoadMapFileTest.cs
+++ Seagull.Visualisation/Assets/Tests/Components/Model/LoadMapFileTest.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using BeardedPlatypus.Functional.NetCDF;
 using NUnit.Framework;
 using PathLib;
 using Seagull.Visualisation.Core.Application.Model;
@@ -14,15 +16,15 @@ namespace Seagull.Visualisation.Tests.Components.Model
         public void LoadMapFile_RetrievesCorrectValues()
         {
             var transformationService = new CoordinateSystemTransformationService();
-            var netcdfStrategy = new ModelRepositoryCreationStrategyNetCDF(transformationService, 
-                                                                           new EPSGCode(3857));
+            var netcdfStrategy = new ModelRepositoryCreationStrategyNetCDF(transformationService,
+                new EPSGCode(3857));
 
             var factory = new ModelRepositoryFactory(new[] {netcdfStrategy});
             IPath modelPath = Paths.Create(TestContext.CurrentContext.TestDirectory)
-                                   .Parent()
-                                   .Parent()
-                                   .Join("Assets", "Tests", "Components", "test-data", "map.nc");
-            
+                .Parent()
+                .Parent()
+                .Join("Assets", "Tests", "Components", "test-data", "map.nc");
+
             Assert.That(factory.CanCreateFor(modelPath));
 
             var modelRepository = factory.Create(modelPath);
@@ -31,11 +33,36 @@ namespace Seagull.Visualisation.Tests.Components.Model
             Assert.That(modelRepository.RetrieveMeshes2D(), Has.Length.EqualTo(1));
 
             IMesh2D mesh2d = modelRepository.RetrieveMeshes2D().First();
-            
+
             Assert.That(mesh2d.RetrieveName(), Is.EqualTo("mesh2d"));
             Assert.That(mesh2d.RetrieveVertices(), Has.Length.EqualTo(36));
             Assert.That(mesh2d.RetrieveEdges(), Has.Length.EqualTo(60));
             Assert.That(mesh2d.RetrieveFaces(), Has.Length.EqualTo(25));
+        }
+
+        [Test]
+        public void QueryTime_Sample()
+        {
+            // Execute the query
+            IPath modelPath = Paths.Create(TestContext.CurrentContext.TestDirectory)
+                .Parent()
+                .Parent()
+                .Join("Assets", "Tests", "Components", "test-data", "map.nc");
+            
+            var query = new TimeQuery();
+
+            var path = modelPath.ToString();
+            Service.Query(path, query);
+
+            // Validate the results
+            var expectedStartTime = DateTime.Parse("2001-01-01 00:00:00 +00:00");
+            Assert.That(query.StartTime, Is.EqualTo(expectedStartTime));
+
+            var expectedTimeSteps =
+                Enumerable.Range(0, 73)
+                    .Select(i => TimeSpan.FromSeconds(i * 1200.0))
+                    .ToList();
+            Assert.That(query.TimeSteps, Is.EqualTo(expectedTimeSteps));
         }
     }
 }
---
+++ Seagull.Visualisation/Assets/Tests/Components/Model/QueryTime.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BeardedPlatypus.Functional.NetCDF;
+
+namespace Seagull.Visualisation.Tests.Components.Model
+{
+    /// <summary>
+    /// The <see cref="TimeQuery"/> implements the <see cref="IQuery"/> to obtain
+    /// the Time component of a NetCDF UGRID file.
+    /// It retrieves the start time and time steps.
+    /// </summary>
+    public class TimeQuery : IQuery
+    {
+        public DateTime StartTime { get; private set; }
+
+        public IList<TimeSpan> TimeSteps { get; private set; }
+
+        public void Execute(IRepository repository)
+        {
+            // Retrieve the id by finding the first component that defines a time component.
+            // In the case of UGRID files, this is a variable that has a standard_name attribute
+            // set to "time"
+            var ids = repository.RetrieveVariablesWithAttributeWithValue("standard_name", "time")
+                .ToList();
+            var id = ids[0];
+            
+            // The time variable should always have a "units" attribute, that defines a string 
+            // as "<time-quantity> since <date>", for example "seconds since 2001-01-01 00:00:00 +00:00"
+            var units = repository.RetrieveVariableAttribute<string>(id, "units");
+            InterpretUnitsString(units.Values.First(), out var timeStep, out var startTime);
+            StartTime = startTime;
+
+            // The "time" variable is a 1D sequence of doubles, we will convert this to time spans
+            // by using the <time-quantity> obtained from the units string.
+            TimeSteps = repository.RetrieveVariableValue<double>(id)
+                .Values
+                .Select(GetToTimeStep(timeStep))
+                .ToList();
+        }
+
+        private static void InterpretUnitsString(string units,
+            out string o,
+            out DateTime startTime)
+        {
+            string[] parts = units.Split(new[] { " since " }, StringSplitOptions.RemoveEmptyEntries);
+
+            // We assume the string is correctly formatted. In production code we might want to add
+            // some more validation here.
+            o = parts[0];
+            startTime = DateTime.Parse(parts[1]);
+        }
+
+        private static Func<double, TimeSpan> GetToTimeStep(string timeStepSize) =>
+            timeStepSize switch
+            {
+                "seconds" => TimeSpan.FromSeconds,
+                "hours" => TimeSpan.FromHours,
+                "days" => TimeSpan.FromDays,
+                _ => throw new ArgumentOutOfRangeException(nameof(timeStepSize), timeStepSize, null)
+            };
+    }
+}
