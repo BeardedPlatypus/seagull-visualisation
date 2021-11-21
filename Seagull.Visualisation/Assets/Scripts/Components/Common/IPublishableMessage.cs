b00e786bb6f9b0b3c95f6ada8dc5c1d1b49a70a0
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-21 21:43:34 +0100

    Add IPublishableMessage

---
+++ Seagull.Visualisation/Assets/Scripts/Components/Common/IPublishableMessage.cs
@@ -0,0 +1,20 @@
+﻿namespace Seagull.Visualisation.Components.Common
+{
+    /// <summary>
+    /// <see cref="IPublishableMessage"/> defines the publish messages, such that
+    /// messages of specific types get published as their own type, while still
+    /// providing a common interface with which to store the messages.
+    /// </summary>
+    public interface IPublishableMessage
+    {
+        /// <summary>
+        /// Publish this <see cref="IPublishableMessage"/> through the message broker.
+        /// </summary>
+        /// <remarks>
+        /// This method needs to be implemented in the actual message (and not a parent
+        /// class), in order to ensure the right type is used.
+        /// </remarks>
+        void Publish();
+
+    }
+}
