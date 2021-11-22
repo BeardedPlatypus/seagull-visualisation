92de3e63fbb85ac88f5a83618a13956b2f6d30e9
BeardedPlatypus <mwtegelaers@gmail.com>
2021-11-22 09:43:42 +0100

    Add RecentProjectServiceWrapper

---
+++ Seagull.Visualisation/Assets/Scripts/Views/MainMenu/RecentProjectsServiceWrapper.cs
@@ -0,0 +1,32 @@
+using Seagull.Visualisation.Core.Application;
+using Seagull.Visualisation.Views.MainMenu.Common;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Seagull.Visualisation.Views.MainMenu
+{
+    /// <summary>
+    /// <see cref="RecentProjectsServiceWrapper"/> provides a wrapper around the
+    /// <see cref="IRecentProjectService"/>, forwarding any relevant messages to
+    /// it.
+    /// </summary>
+    public sealed class RecentProjectsServiceWrapper : MonoBehaviour
+    {
+        private IRecentProjectService _service;
+        
+        [Inject]
+        private void Init(IRecentProjectService recentProjectService)
+        {
+            _service = recentProjectService;
+        }
+
+        private void Start()
+        {
+            MessageBroker.Default.Receive<UpdateRecentProjectMessage>()
+                                 .Select(x => x.RecentProject)
+                                 .Subscribe(_service.UpdateRecentProject)
+                                 .AddTo(this);
+        }
+    }
+}
